47832fa70b066e5d684d43d4f856a6050a7e2051
ParameshGarimella <101395104+VenkataParamesh@users.noreply.github.com>
2022-06-04 16:24:01 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer style="background-color:#3D3D3D;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2022 VTOP ONLINE COURSE REGISTRATION |   BY : <a href="https://vitap.ac.in/" target="_blank">VIT-AP STUDENTS</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ admin/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero" style="background-color:#6378DF;color:white; padding:15px">
+        <div class="container" style="background-color:#6378DF;color:white; padding:50px margin:10px">
+            <div class="navbar-header" style="background-#6378DF:black;color:white; margin:1px">
+               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#" style="color:#fff; font-size:30px;4px; line-height:40px; ">
+
+                   VTOP ONLINE COURSE REGISTRATION
+                </a>
+
+            </div>
+
+            <div class="left-div">
+             <center><img src="assets/img/vitap.png" class="img-responsive pull-left" alt="User Image" width="100"></center> 
+        </div>
+            </div>
+
+        </div>
